8ddf20204eab57115029106a79fd1d3c61a803be
Yash Godara <yash17godara@gmail.com>
2023-09-22 15:52:30 +0530

    Update

--- cjs/q1java.java
+++ cjs/q1java.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 class Solution {
-	
-	public static void main(String args[]) {
-		
-	}
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int l, b;
+    l = sc.nextInt();
+    b = sc.nextInt();
+    int area = (l * b);
+    System.out.println(area);
+    sc.close();
+  }
 }
