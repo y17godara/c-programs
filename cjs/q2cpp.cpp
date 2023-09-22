53b0ec0a63da876a808a0cc0d3b7f03e32fc5594
Yash Godara <yash17godara@gmail.com>
2023-09-22 16:09:14 +0530

    q2

---
+++ cjs/q2cpp.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+  std::string name;
+  int age;
+
+  std::cin >> name;
+  std::cin >> age;
+
+  std::cout << "The name of the person is " << name << " and the age is " << age << "." << std::endl;
+
+  return 0;
+}
