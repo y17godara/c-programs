3998190157bc02be08ff89830d5e211861d4f2db
Yash Godara <yash17godara@gmail.com>
2023-09-24 17:03:32 +0530

    Find Character Case

---
+++ cjs/q6cpp.cpp
@@ -0,0 +1,18 @@
+// Find Character Case
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch;
+    cin >> ch;
+    if(ch>=65 && ch<=90){
+        cout << "1";
+        return 1;
+    }
+    else if(ch>=97 && ch<=122) {
+        cout << "0";
+        return 0;
+    }
+    else cout << "-1";
+    return -1;
+}
