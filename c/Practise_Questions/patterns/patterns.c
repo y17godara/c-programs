3a2b89ecb9cefc20fc118519415ba75d2b02fc00
Yash Godara <yash17godara@gmail.com>
2023-10-05 17:48:19 +0530

    printHallowRectangle

--- c/Practise_Questions/patterns/patterns.c
+++ c/Practise_Questions/patterns/patterns.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
+void printHallowRectangle(int row, int col, char ch){
+  for(int i=0; i<=row-1; i++) {
+    for(int j=0; j<=col-1; j++) {
+      if(i==0 || i==row-1 || j==0 || j==col-1) {
+        printf("%c", ch);
+      } else {
+        printf(" ");
+      }
+    }
+    printf("\n");
+  }
+}
+
 int main() {
-  printf("Hello World!");
+  printHallowRectangle(10, 14, '#');
   return 0;
 } 
