54cedc09118251a4f8a2bd9649bc5c7833ebdf68
Yash Godara <yash17godara@gmail.com>
2023-10-17 17:04:18 +0530

    Strings Intro

---
+++ java_basics/Strings.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Strings {
+    public static void main(String args[]){
+         /* 
+        char arr[] = {'a','b','c','d','e'};
+        String str = "abcde";
+        String str2 = new String("adcde");
+
+        // strings are immutable
+
+        Scanner sc = new Scanner(System.in);
+
+        String name;
+        name = sc.nextLine(); // input string
+        System.out.println(name);
+
+        // length of string
+        System.out.println(name.length());
+        
+        // concatination
+        String firstName = "John";
+        String lastName = "Doe";
+        
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+        */
+        
+    }
+}
