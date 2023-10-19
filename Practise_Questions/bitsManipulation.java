98370f8eae59c204a8bbbdd1fc329a22e6149bbb
Yash Godara <yash17godara@gmail.com>
2023-10-19 15:37:54 +0530

    bitsManipulation

---
+++ Practise_Questions/bitsManipulation.java
@@ -0,0 +1,32 @@
+public class bitsManipulation {
+    public static int getBit(int num, int k) {
+        int bitMask = 1 << k;
+        if ((num & bitMask) == 0) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    public static int setBit(int num, int k) {
+        int bitMask = 1 << k;
+        return num | bitMask;
+    }
+
+    public static int clearBit(int num, int k) {
+        int bitMask = ~(1 << k);
+        return num & bitMask;
+    }
+
+    public static int updateBit(int num, int k, int n) {
+        
+    }
+
+    public static void main(String args[]) {
+        int x = 10;
+        System.out.println(getBit(x, 3));
+        System.out.println(setBit(x, 3));
+        System.out.println(clearBit(x, 3));
+        System.out.println(updateBit(x, 3, 0));
+    }
+}
