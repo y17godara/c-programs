1cebf1dab8c68e3404852adc6fb30c782f4055eb
Yash Godara <yash17godara@gmail.com>
2023-10-22 14:03:27 +0530

    Update OOPsBasic.java

--- Practise_Questions/OOPsBasic.java
+++ Practise_Questions/OOPsBasic.java
@@ -1,17 +1,48 @@
+/* Q1 */
+// class Students {
+//     String name;
+//     int marks;
+// }
 
-class Students {
-    String name;
-    int marks;
-}
+// public class OOPsBasic {
+//     public static void main(String args[]) {
+//         Students s1 = new Students();
+//         //
+//         s1.marks = 80;
+//         s1.name = "Ramesh";
 
-public class OOPsBasic {
-    public static void main(String args[]) {
-        Students s1 = new Students();
-        //
-        s1.marks = 80;
-        s1.name = "Ramesh";
+//         System.out.println(s1.marks);
+//         System.out.println(s1.name);
+//     }
+// }
+// Ans
+//         s1.marks = 80;
+//         s1.name = "Ramesh";
 
-        System.out.println(s1.marks);
-        System.out.println(s1.name);
-    }
-}
+//         System.out.println(s1.marks);
+//         System.out.println(s1.name);
+
+/* Q2 */
+// class Person {
+//     String name;
+//     int weight;
+// }
+
+// class Students extends Person {
+//     int rollNumber;
+//     String schoolName;
+// }
+// Ans
+// Peron class can access only name and weight;
+
+/* Q3 */
+// Private, protected. public, default, Which classes not present
+// Ans
+// Protected and Private Classes
+
+/* Q4 */
+// class Vehicle{}
+// class Car extends Vehicle {}
+// which of the following is a correct statement? (both classes in same package)
+// Ans
+// Car c = new Car();
