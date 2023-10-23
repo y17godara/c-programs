177a96e38e632ffc3c72da024e2812ace9498dfa
Yash Godara <yash17godara@gmail.com>
2023-10-23 21:52:51 +0530

    Basic Recursion

---
+++ Practise_Questions/Recursion.java
@@ -0,0 +1,14 @@
+public class Recursion {
+    public static void printDec(int n){
+        if(n == 0){
+            System.out.println(0);
+            return;
+        }
+        System.out.println(n);
+        printDec(n-1);
+    }
+    public static void main(String args[]){
+        int n = 9;
+        printDec(n);
+    }
+}
