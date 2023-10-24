6164e214b29894534dfbfc793f78c1ca7db87a4c
Yash Godara <yash17godara@gmail.com>
2023-10-24 17:27:46 +0530

    Update Recursion.java

--- Practise_Questions/Recursion.java
+++ Practise_Questions/Recursion.java
@@ -1,13 +1,14 @@
 public class Recursion {
-    public static void printDec(int n){
-        if(n == 0){
+    public static void printDec(int n) {
+        if (n == 0) {
             System.out.println(0);
             return;
         }
         System.out.println(n);
-        printDec(n-1);
+        printDec(n - 1);
     }
-    public static void main(String args[]){
+
+    public static void main(String args[]) {
         int n = 9;
         printDec(n);
     }
