6e9f479ca384d9ac0a8f16d09e18e49482cf4af4
Yash Godara <yash17godara@gmail.com>
2023-10-24 19:58:05 +0530

    arraySorted with Recursion

--- Practise_Questions/RecursionBasics.java
+++ Practise_Questions/RecursionBasics.java
@@ -17,6 +17,7 @@ public class RecursionBasics {
         }
     }
 
+    // function: get fabonacci of a number
     public static int fabonacci(int n) {
 
         if (n == 0 || n == 1) {
@@ -29,13 +30,31 @@ public class RecursionBasics {
         return fib;
     }
 
+    // function: check if array is sorted
+    public static boolean isArraySorted(int arr[], int index) {
+
+        if(arr.length-1 == index){
+            return true;
+        }
+
+        if(arr[index] > arr[index+1]){
+            return false;
+        }
+        return isArraySorted(arr, index+1);
+    }
+
     // main
     public static void main(String args[]) {
 
-        int num = 9;
-
+        // int num = 26;
         // System.out.println(getFactorial(num));
         // System.out.println(getSumOfXNaturalNumber(num));
-        System.out.println(fabonacci(num));
+        // System.out.println(fabonacci(num));
+
+        // int arr[] = {2,4,6,8,8};
+        // boolean isSorted = isArraySorted(arr, 0);
+        // System.out.println(isSorted);
+
+        
     }
 }
