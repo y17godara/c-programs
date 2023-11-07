c6a097440c47d3f6d3233ff9ca586abfefdb9089
Yash Godara <yash17godara@gmail.com>
2023-11-07 19:36:04 +0530

    Update ArrayListExample.java

--- java_basics/ArrayListExample.java
+++ java_basics/ArrayListExample.java
@@ -1,7 +1,8 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class ArrayListExample {
-    public static void main(String[] args) {
+
+    public static void part1() {
         // Create an ArrayList of Integers
         ArrayList<Integer> numbers = new ArrayList<>();
 
@@ -24,5 +25,37 @@ public class ArrayListExample {
         // Delete elements from the ArrayList
         numbers.remove(2);
         System.out.println(numbers);
+    }
+
+    public static void part2(){
+        ArrayList<Integer> nums = new ArrayList<>();
+
+        // print reverse 
+        for (int i = 0; i < 10; i++) {
+            nums.add(i);
+        }
+        System.out.println("Print reverse");
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            System.out.println(nums.get(i));
+        }
+
+        // find maximum
+        int max = nums.get(0);
+        for (int i = 1; i < nums.size(); i++) {
+            max = Math.max(max, i);
+        }
+        System.out.println("Max: " + max);
+
+        // swap 2 elements
+        int temp = nums.get(0);
+        nums.set(0, nums.get(1));
+        nums.set(1, temp);
+        System.out.println(nums);
+    }
+
+    
+
+    public static void main(String[] args) {
+        part2();
     }
 }
