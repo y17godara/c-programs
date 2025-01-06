6f45798cef4a41be4ae1104153560b75cdf8cdc4
Yash Godara <yash17godara@gmail.com>
2025-01-06 17:36:10 +0530

    Update start-patterns.cpp

--- s2/start-patterns.cpp
+++ s2/start-patterns.cpp
@@ -1,39 +1,24 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void printPatter1(int n) {
-    for (int i = 0; i<n; i++) {
-      
-         for (int j =0; j<n-i-1; j++)
-        {
-            cout <<" ";
-        }
-       
-        for(int j=0;j< 2*i+1;j++){
-            
-            cout<<"*";
-        }
-        
-         for (int j =0; j<n-i-1; j++)
-        {
-            cout <<" ";
-        }
+void printPatter1(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
 
-        cout << endl;
-    }
-}
-
-void nStarTriangle(int n) {
-    for(int i=n; i>=0; i--){
-        for(int j=n-i+1; j>1; j--) {
+        for (int j = 0; j < n - i - 1; j++)
+        {
             cout << " ";
         }
 
-        for(int j=0; j<=2*i; j++) {
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
+
             cout << "*";
         }
 
-        for(int j=n-i+1; j>1; j--) {
+        for (int j = 0; j < n - i - 1; j++)
+        {
             cout << " ";
         }
 
@@ -41,27 +26,57 @@ void nStarTriangle(int n) {
     }
 }
 
-void nStarDiamond(int n) {
+void nStarTriangle(int n)
+{
+    for (int i = n; i >= 0; i--)
+    {
+        for (int j = n - i + 1; j > 1; j--)
+        {
+            cout << " ";
+        }
+
+        for (int j = 0; j <= 2 * i; j++)
+        {
+            cout << "*";
+        }
+
+        for (int j = n - i + 1; j > 1; j--)
+        {
+            cout << " ";
+        }
+
+        cout << endl;
+    }
+}
+
+void nStarDiamond(int n)
+{
     cout << "Diamond Pattern" << endl;
 
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<n-i-1; j++) {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
             cout << " ";
         }
 
-        for(int j=0; j<2*i+1; j++) {
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
             cout << "*";
         }
 
         cout << endl;
     }
 
-    for (int i=n-1; i>0; i--) {
-        for(int j=0; j<n-i; j++) {
+    for (int i = n - 1; i > 0; i--)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
             cout << " ";
         }
 
-        for(int j=0; j<2*i-1; j++) {
+        for (int j = 0; j < 2 * i - 1; j++)
+        {
             cout << "*";
         }
 
@@ -74,17 +89,22 @@ void nStarDiamond(int n) {
 // ***
 // **
 // *
-void mStarTriangle(int n) {
+void mStarTriangle(int n)
+{
     // Write your code here.
-    for(int i=0; i<=n-2; i++) {
-        for(int j=0; j<=i; j++) {
+    for (int i = 0; i <= n - 2; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
             cout << "*";
         }
         cout << endl;
     }
 
-    for(int i=n; i>0; i--){
-        for(int j=i-1; j>=0; j--) {
+    for (int i = n; i > 0; i--)
+    {
+        for (int j = i - 1; j >= 0; j--)
+        {
             cout << "*";
         }
         cout << endl;
@@ -92,37 +112,45 @@ void mStarTriangle(int n) {
 }
 
 // Input: ‘N’ = 3
-// Output: 
+// Output:
 // 1
 // 0 1
 // 1 0 1
-void nBinaryTriangle(int n) {
-    for(int i=0; i<n; i++){
+void nBinaryTriangle(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
         int s = i % 2;
-        for(int j=0; j<=i; j++) {
+        for (int j = 0; j <= i; j++)
+        {
             cout << s;
-            s = 1-s;
+            s = 1 - s;
         }
         cout << endl;
     }
 }
 
 // Input: ‘N’ = 3
-// Output: 
+// Output:
 // 1         1
 // 1 2     2 1
 // 1 2 3 3 2 1
-void numberCrown(int n) {
-    for(int i=1; i<=n; i++) {
-        for(int j=1; j<=i; j++){
+void numberCrown(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
             cout << j << " ";
         }
 
-        for (int j = 1; j <= 2 * (n - i); j++) {
+        for (int j = 1; j <= 2 * (n - i); j++)
+        {
             cout << " "; // Two spaces for alignment
         }
 
-        for(int j=i; j>=1; j--){
+        for (int j = i; j >= 1; j--)
+        {
             cout << j << " ";
         }
 
@@ -131,14 +159,17 @@ void numberCrown(int n) {
 }
 
 // Input: ‘N’ = 3
-// Output: 
+// Output:
 // 1
 // 2 3
 // 4 5 6
-void nNumberTriangle(int n) {
+void nNumberTriangle(int n)
+{
     int s = 1;
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<=i; j++) {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
             cout << s << " ";
             s += 1;
         }
@@ -147,7 +178,7 @@ void nNumberTriangle(int n) {
 }
 
 // Input: ‘N’ = 3
-// Output: 
+// Output:
 // A
 // A B
 // A B C
@@ -161,59 +192,177 @@ void nNumberTriangle(int n) {
 // }
 
 // #2
-void nLetterTriangle(int n) {
-    for (int i = 1; i <= n; i++) { // Outer loop for rows
-        for (char ch = 'A'; ch <= 'A' + i - 1; ch++) { // Inner loop for letters
+void nLetterTriangle(int n)
+{
+    for (int i = 1; i <= n; i++)
+    { // Outer loop for rows
+        for (char ch = 'A'; ch <= 'A' + i - 1; ch++)
+        { // Inner loop for letters
             cout << ch << " ";
         }
         cout << endl; // Move to the next line
     }
 }
 
-// 
+//
 // A B C
 // A B
 // A
-void nLetterTriangle2(int n) {
+void nLetterTriangle2(int n)
+{
     // Write your code here.
 }
 
-
-// 
+//
 
 /*
 Input: ‘N’ = 3
-Output: 
+Output:
 C
-C B 
+C B
 C B A
 */
-void alphaTriangle(int n) {
+void alphaTriangle(int n)
+{
     // Write your code here.
-    for(int i=1; i<=n; i++){
+    for (int i = 1; i <= n; i++)
+    {
         // for(int j=1; j<=i; j++){
         //     cout << char('D'-j) << " ";
         // }
-        for (int j=1; i>=j; j++){
-              cout << char('A' + (n - j)) << " ";
+        for (int j = 1; i >= j; j++)
+        {
+            cout << char('A' + (n - j)) << " ";
         }
         cout << endl;
     }
 }
 
 // Input: ‘N’ = 3
+// Output:
+// * * * * * *
+// * *     * *
+// *         *
+// *         *
+// * *     * *
+// * * * * * *
+void symmetry(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        int spaces = 0;
+
+        for (int k = 0; k < n - i; k++)
+        {
+            cout << "* ";
+        }
+
+        for (int s = 0; s < i * 2; s++)
+        {
+            cout << "  ";
+        }
+
+        for (int k = 0; k < n - i; k++)
+        {
+            cout << "* ";
+        }
+
+        spaces += 2;
+        cout << endl;
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        int spaces = i * 2;
+
+        for (int k = 0; k < i; k++)
+        {
+            cout << "*" << " ";
+        }
+
+        for (int s = n * 2; s > spaces; s--)
+        {
+            cout << "  ";
+        }
+
+        for (int k = 0; k < i; k++)
+        {
+            cout << "* ";
+        }
+
+        spaces += 2;
+        cout << endl;
+    }
+}
+
+/*
+Example:
+Input: ‘N’ = 3
+
+Output: 
+*         *
+* *     * *
+* * * * * *
+* *     * *
+*         *
+*/
+void symmetry2(int n) {
+    for (int i = n - 1; i >= 0; i--) {
+        // Print leading spaces
+        int spaces = 4;
+
+        // Print numbers in ascending order
+        for (int j = 1; j <= n-i; j++) {
+            cout << "*" << " ";
+        }
+
+        for (int s = i*2; s > 0; s--) {
+            cout << " " << " ";
+        }
+
+        for (int j = 1; j <= n-i; j++) {
+            cout << "*" << " ";
+        }
+
+        spaces -= 2;
+        cout << endl; // Move to the next line
+    }
+
+    for (int i = 1; i <= n - 1; i++) {
+        // Print leading spaces
+        int spaces = 4;
+
+        // Print numbers in ascending order
+        for (int j = 1; j <= n-i; j++) {
+            cout << "*" << " ";
+        }
+
+        for (int s = i*2; s > 0; s--) {
+            cout << " " << " ";
+        }
+
+        for (int j = 1; j <= n-i; j++) {
+            cout << "*" << " ";
+        }
+
+        spaces -= 2;
+        cout << endl; // Move to the next line
+    }
+}
+
+// Input: ‘N’ = 4
 // Output: 
-// * * * * * * 
-// * *     * * 
-// *         * 
-// *         * 
-// * *     * * 
-// * * * * * * 
-void symmetry(int n) {
+// ****
+// *  *
+// *  *
+// ****
+void getStarPattern(int n) {
     // Write your code here.
 }
 
-int main() {
+
+int main()
+{
     // printPatter1(5);
     // nStarTriangle(5);
     // nStarDiamond(5);
@@ -225,7 +374,9 @@ int main() {
     // nLetterTriangle2(3);
 
     // alphaTriangle(3);
-    symmetry(3);
+    // symmetry(5);
+    // symmetry2(3);
+    getStarPattern(4);
 
     return 0;
 }
