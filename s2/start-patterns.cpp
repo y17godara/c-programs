94bf58b00bddf2e94d6549298a480f73d7cb0aa2
Yash Godara <yash17godara@gmail.com>
2025-01-06 18:06:02 +0530

    Update start-patterns.cpp

--- s2/start-patterns.cpp
+++ s2/start-patterns.cpp
@@ -389,7 +389,28 @@ void getStarPattern(int n) {
 // 4444444
 
 void getNumberPattern(int n) {
-
+  for(int i=0;i<2*n-1;i++){
+         
+         // inner loop for no. of columns.
+         for(int j=0;j<2*n-1;j++){
+             
+             // Initialising the top, down, left and right indices of a cell.
+             int top = i;
+             int bottom = j;
+             int right = (2*n - 2) - j;
+             int left = (2*n - 2) - i;
+             
+             // Min of 4 directions and then we subtract from n
+             // because previously we would get a pattern whose border
+             // has 0's, but we want with border N's and then decreasing inside.
+             cout<<(n- min(min(top,bottom), min(left,right)))<<" ";
+         }
+         
+         // As soon as the numbers for each iteration are printed, we move to the
+         // next row and give a line break otherwise all numbers
+         // would get printed in 1 line.
+         cout<<endl;
+     }
 }
 
 int main()
