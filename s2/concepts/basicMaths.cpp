ad6bb4b34a863d16fcd8119ec108829ae860d7ce
Yash Godara <yash17godara@gmail.com>
2025-01-11 20:36:53 +0530

    Update basicMaths.cpp

--- s2/concepts/basicMaths.cpp
+++ s2/concepts/basicMaths.cpp
@@ -1,6 +1,29 @@
+#include<iostream>
+using namespace std;
+
 /*
     Basic Maths Concept
 */
 
 // 7789 % 10 = 9
 // 7789 / 10 = 778
+
+// Count Digit
+int countDigits(int n){
+	int cnt = 0;
+
+    while (n > 0) {
+        int lastDigit = n % 10;
+        cnt += 1;
+        n = n / 10;
+    }
+
+    cout << "No of Digits: " << cnt;
+    return cnt
+}
+
+int main(){
+    countDigits(35);
+
+    return 0;
+}
